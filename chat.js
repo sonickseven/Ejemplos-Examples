374698f60639eeef51b690ea8ce196ce944201d3
SonickSeven <sonickfaber7@yahoo.es>
2013-09-11 10:31:04 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,6 +1,28 @@
-//chatBoxTitle es el identificador unico de cada chat por que son ventana por usuario, varias ventanas
-socket.on('dataU', function(dato){
-	socket.emit('historychat', {user:'68458'+userGlobal, userNick: userNick, userRep: dato.doc});//envia los datos para el histori
+var socket = io.connect('http://127.0.0.1:3000');
+
+socket.on('connect', function(){
+	listen(userNick);//user nick es mi nick en la pagina
+});
+
+function listen(user){//es una funcion futura todabia no tiene uno pero escucha cuando me envian mensaje
+	socket.on('chat'+user, function(datos){
+		alert('Tienes un mensaje de '+datos.user);
+	});
+}
+
+function createChat(chatboxtitle){//chatboxtitle es el identificador unico de cada ventana
+	if ($("#chatbox_"+chatboxtitle).length > 0) {
+		if ($("#chatbox_"+chatboxtitle).css('display') == 'none') {
+			$("#chatbox_"+chatboxtitle).fadeIn(1000);
+		}
+		console.log('ya existe');
+		return;
+	}
+	socket.emit('chatCreate', chatboxtitle);//envia los dato principales del usuario para esta ventana
+}
+
+socket.on('dataU', function(dato){//recoje todos los datos del usuario y tambien se vuelve a recivir chatboxtitle
+	socket.emit('historychat', {user:'68458'+userGlobal, userNick: userNick, userRep: dato.doc});//envia los datos para el historial del chat
 	var cont=se('body');
 	var chatt=document.createElement('section');
 	chatt.id="chatbox_"+dato.chatBoxTitle;
@@ -14,7 +36,7 @@ socket.on('dataU', function(dato){
 				'<input type="text" class="" requiered placeholder="Respondele" data-user="54878'+dato.doc+'" autofocus/>';
 	cont.appendChild(chatt);
 	socket.on('hismsg', function(datos){//recive todas las historias del mensaje
-		console.log(datos.length, dato.chatBoxTitle);//me muestra un incremento por cada chat que abro
+		console.log(datos.length, dato.chatBoxTitle);
 		for(var i=datos.length-1; i>=0; i--){//llena el chat con todos los mensajes
 			$("#chatbox_"+dato.chatBoxTitle+' > article > div').append('<h4>'+datos[i].enviado_por+'</h4><span>'+datos[i].time+'</span>'+
 						'<p>'+datos[i].mensaje+'</p>');
@@ -22,11 +44,11 @@ socket.on('dataU', function(dato){
 		$("#chatbox_"+dato.chatBoxTitle+' > article').animate({scrollTop: $('.chat > article > div').height()}, 800);
 	});
 	$('.chat').fadeIn(800);
-		//fixed('.chat');
-		$('.chat').draggable();
-		$("#chatbox_"+dato.chatBoxTitle+' > span').on('click', function(){
-			$("#chatbox_"+dato.chatBoxTitle).fadeOut(800, function(){
-				se('body').removeChild(se("#chatbox_"+dato.chatBoxTitle));
-			});
+	//fixed('.chat');
+	$('.chat').draggable();
+	$("#chatbox_"+dato.chatBoxTitle+' > span').on('click', function(){
+		$("#chatbox_"+dato.chatBoxTitle).fadeOut(800, function(){
+			se('body').removeChild(se("#chatbox_"+dato.chatBoxTitle));
 		});
+	});
 });
