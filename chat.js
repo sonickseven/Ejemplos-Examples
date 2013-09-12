301af68223eb3b7c09131e5f035737af349c76ca
SonickSeven <sonickfaber7@yahoo.es>
2013-09-11 20:37:01 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,28 +1,10 @@
 var socket = io.connect('http://127.0.0.1:3000');
+var d=new Date();
 
-socket.on('connect', function(){
-	listen(userNick);//user nick es mi nick en la pagina
-});
-
-function listen(user){//es una funcion futura todabia no tiene uno pero escucha cuando me envian mensaje
-	socket.on('chat'+user, function(datos){
-		alert('Tienes un mensaje de '+datos.user);
-	});
-}
-
-function createChat(chatboxtitle){//chatboxtitle es el identificador unico de cada ventana
-	if ($("#chatbox_"+chatboxtitle).length > 0) {
-		if ($("#chatbox_"+chatboxtitle).css('display') == 'none') {
-			$("#chatbox_"+chatboxtitle).fadeIn(1000);
-		}
-		console.log('ya existe');
-		return;
-	}
-	socket.emit('chatCreate', chatboxtitle);//envia los dato principales del usuario para esta ventana
-}
-
-socket.on('dataU', function(dato){//recoje todos los datos del usuario y tambien se vuelve a recivir chatboxtitle
-	socket.emit('historychat', {user:'68458'+userGlobal, userNick: userNick, userRep: dato.doc});//envia los datos para el historial del chat
+$(document).on('ready', function(){
+	
+	socket.on('dataU', function(dato){
+	socket.emit('historychat', {user:'68458'+userGlobal, userNick: userNick, userRep: dato.doc});//envia los datos para el histori
 	var cont=se('body');
 	var chatt=document.createElement('section');
 	chatt.id="chatbox_"+dato.chatBoxTitle;
@@ -33,22 +15,57 @@ socket.on('dataU', function(dato){//recoje todos los datos del usuario y tambien
 					'<div>'+
 					'</div>'+
 				'</article>'+
-				'<input type="text" class="" requiered placeholder="Respondele" data-user="54878'+dato.doc+'" autofocus/>';
+				'<input type="text" class="msg" requiered placeholder="Respondele" data-user="54878'+dato.doc+'" autofocus/>';
 	cont.appendChild(chatt);
-	socket.on('hismsg', function(datos){//recive todas las historias del mensaje
-		console.log(datos.length, dato.chatBoxTitle);
+	socket.once('hismsg', function(datos){//recive todas las historias del mensaje
 		for(var i=datos.length-1; i>=0; i--){//llena el chat con todos los mensajes
 			$("#chatbox_"+dato.chatBoxTitle+' > article > div').append('<h4>'+datos[i].enviado_por+'</h4><span>'+datos[i].time+'</span>'+
 						'<p>'+datos[i].mensaje+'</p>');
 		}
-		$("#chatbox_"+dato.chatBoxTitle+' > article').animate({scrollTop: $('.chat > article > div').height()}, 800);
+		$("#chatbox_"+dato.chatBoxTitle+' > article').animate({scrollTop: $("#chatbox_"+dato.chatBoxTitle+'> article > div').height()}, 800);
 	});
 	$('.chat').fadeIn(800);
 	//fixed('.chat');
 	$('.chat').draggable();
+	$("#chatbox_"+dato.chatBoxTitle+' > input').on('keyup', escribir);
 	$("#chatbox_"+dato.chatBoxTitle+' > span').on('click', function(){
 		$("#chatbox_"+dato.chatBoxTitle).fadeOut(800, function(){
 			se('body').removeChild(se("#chatbox_"+dato.chatBoxTitle));
 		});
 	});
 });
+
+socket.on('chat'+userNick, function(datos){
+		alert('Tienes un mensaje de '+datos.user);
+	});
+
+});
+socket.on('connect', function(){
+	listen(userNick);
+});
+
+function se(tag){
+	return document.querySelector(tag);
+}
+function listen(user){
+	
+}
+
+function createChat(chatboxtitle){
+	if ($("#chatbox_"+chatboxtitle).length > 0) {
+		if ($("#chatbox_"+chatboxtitle).css('display')==='none') {
+			$("#chatbox_"+chatboxtitle).fadeIn(1000);
+		}
+		console.log('ya existe');
+		return;
+	}
+	socket.emit('chatCreate', chatboxtitle);
+}
+function escribir(e){
+	if(e.which===13){
+		if($(this).val()!==''){
+			socket.emit('mgss', {time:d.toLocaleTimeString(), date:d.toLocaleDateString(), usaer: $(this).data('user'), mens: $(this).val(), nick:userNick, userstart: '6841844'+userGlobal});
+			$(this).val('');
+		}
+	}
+}
