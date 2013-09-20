5dcfc0b721afc262bcdf22fc1bdac0b45ff4efcf
SonickSeven <sonickfaber7@yahoo.es>
2013-09-19 23:38:28 -0500

    Update problemcallback.js

--- problemcallback.js
+++ problemcallback.js
@@ -6,7 +6,7 @@ function histori(dato, callback){
 		for (var i = 0; i < a.length; i++){//por cada reesultadode la consulta
             var historia=a[i].historia;//la nombro para que no haya un futuro error
             slt.selectEspecific(conexion, ['nick', 'nombres'], 'usuario', 'cod', a[i].seguido, 'cod', '', 0, 1, function(user){
-                console.log(historia+' de '+user[0].nick);
+                console.log(historia+' de '+user[0].nick);//mostraria una historia con su creador !!
                 //userObj=user;
             });
         };
@@ -14,3 +14,13 @@ function histori(dato, callback){
     console.log(historias);//muestra {}
 	callback(null);// este lo pongo ya cuando este terminado
 }
+
+//resultado de esa consulta
+//Olle Bratt Pit no eres mi padre!!!  fuck yeah de SonickSeven
+//Olle Bratt Pit no eres mi padre!!!  fuck yeah de noa2
+//Olle Bratt Pit no eres mi padre!!!  fuck yeah de SonickSeven
+//Olle Bratt Pit no eres mi padre!!!  fuck yeah de SonickSeven
+//Olle Bratt Pit no eres mi padre!!!  fuck yeah de marbel93
+//Olle Bratt Pit no eres mi padre!!!  fuck yeah de marbel93
+
+//muestra simepre la misma historia pero el que cambia es el usuario esta mal :(
